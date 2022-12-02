df04ce9cb460063310c8e4de61fcfa0454bed499
Clément Jouin <95017825+ClementEcNantes@users.noreply.github.com>
2022-12-02 10:56:38 +0100

    bateau

--- Bateau.cpp
+++ Bateau.cpp
@@ -1,1 +1,18 @@
 #include "Bateau.hpp"
+
+Bateau :: Bateau(int vitesseMax=0, int nbPlaces=1, int occupants=0){
+
+    this->vitesseMax_ = vitesseMax;
+    this->nbPlaces_ = nbPlaces;
+    this->occupants_ = occupants;
+
+}
+
+void Bateau :: demarrer(){
+
+}
+    
+void Bateau :: arreter(){
+    
+}
+    
