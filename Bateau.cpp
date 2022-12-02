d50f5b0e8754bb0215b69557a1fb901cd04fac71
Clément Jouin <95017825+ClementEcNantes@users.noreply.github.com>
2022-12-02 11:22:26 +0100

    bateau

--- Bateau.cpp
+++ Bateau.cpp
@@ -1,6 +1,6 @@
 #include "Bateau.hpp"
 
-Bateau :: Bateau(int vitesseMax=0, int nbPlaces=1, int occupants=0){
+Bateau :: Bateau(int vitesseMax, int nbPlaces1, int occupants0) : Vehicule(int vitesseMax, int nbPlaces, int occupants){
 
     this->vitesseMax_ = vitesseMax;
     this->nbPlaces_ = nbPlaces;
@@ -9,10 +9,10 @@ Bateau :: Bateau(int vitesseMax=0, int nbPlaces=1, int occupants=0){
 }
 
 void Bateau :: demarrer(){
-
+    cout << "En avant fluffycoquin !" << endl;
 }
     
 void Bateau :: arreter(){
-    
+    cout << "On arrête tout fluffycoquin !" << endl;
 }
     
