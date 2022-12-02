77208f178ce1e536a877fa6e01535c6a57276666
yadon95 <82603489+yadon95@users.noreply.github.com>
2022-12-02 10:31:46 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <iostream>
 
+#include "Vehicule.hpp"
+
 using namespace std;
 
 int main(){
     cout << "Hello les loosers !" << endl;
+
+    try {
+        Vehicule * Vehicule = new Vehicule(0, 0, 0);
+    }
+
     return 0;
 }
