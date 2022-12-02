0dd751b601a4d3dfbb6cbf192b5042b205667166
Clément Jouin <95017825+ClementEcNantes@users.noreply.github.com>
2022-12-02 10:41:38 +0100

    git

--- Bateau.hpp
+++ Bateau.hpp
@@ -1,14 +1,18 @@
 #ifndef Bateau_hpp
 #define Bateau_hpp
+#include "Vehicule.hpp"
 
 class Bateau : public Vehicule
 {
 protected:
 
-    
 public:
 
+    Bateau(int vitesseMax=0, int nbPlaces=1, int occupants=0);
 
+    // Methodes
+    void demarrer();
+    void arreter();
 
 };
 #endif /* Bateau_hpp */
